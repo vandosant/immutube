ec7332466f121ab294bef0e666a183e74c6e098e
vandosant <sjskender@gmail.com>
2016-07-12 12:49:48 -0600

    Log valueStream of input

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,8 @@ define([
 , 'Maybe'
 , 'player'
 , 'bacon'
-], function($, _, P, Maybe, Player, bacon) {
+, 'io'
+], function($, _, P, Maybe, Player, bacon, io) {
   'use strict';
 
   // HELPERS ///////////////////////////////////////////////////////////////////////////
@@ -15,10 +16,21 @@ define([
   var log = function(x) { console.log(x); return x; }
   var fork = _.curry(function(f, future) { return future.fork(log, f); })
   var setHtml = _.curry(function(sel, x) { return $(sel).html(x); });
-
+  io.extendFn();
 
   // PURE //////////////////////////////////////////////////////////////////////////////
+  var listen = _.curry(function(type, el) {
+    return Bacon.fromEventTarget(el, type);
+  });
+  // getDom :: String -> IO Dom
+  var getDom = $.toIO();
+
+  // keypressStream :: Dom -> EventStream DomEvent
+  var keypressStream = listen('keyup');
+
+  // valueStream :: Dom -> EventStream String
+  var valueStream = compose(map(compose(_.get('value'), _.get('target'))), keypressStream);
 
   // IMPURE ////////////////////////////////////////////////////////////////////////////
-
+  getDom('#search').map(valueStream).runIO().onValue(log);
 });
